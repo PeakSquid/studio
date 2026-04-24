511966eb24f2b0c49662ba2ee61f868fa55d94b1
Jaxon <jaxonsferra@gmail.com>
2026-04-24 01:20:46 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5049895982-52aae",
+  "appId": "1:949667437987:web:b87e1b13fe6ae6ddfcce16",
+  "apiKey": "AIzaSyARSMy9ULfcOnDhYHMOa5h0JUY2VNVZsp8",
+  "authDomain": "studio-5049895982-52aae.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "949667437987"
+};
